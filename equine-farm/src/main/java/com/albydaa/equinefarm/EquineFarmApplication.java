52a9177794c30af4cea808e1ca07494e6bb7085a
Hamza <hamza.azeem2013.ha@gmail.com>
2024-03-18 03:31:12 +0200

    porject setup

---
+++ equine-farm/src/main/java/com/albydaa/equinefarm/EquineFarmApplication.java
@@ -0,0 +1,13 @@
+package com.albydaa.equinefarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquineFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquineFarmApplication.class, args);
+	}
+
+}
---
+++ equine-farm/src/test/java/com/albydaa/equinefarm/EquineFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albydaa.equinefarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquineFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
